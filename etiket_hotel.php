f90bde26c880d0730525e52ff3b3e2de16d31374
Vicxendria <superuser@jalurkerja.com>
2022-11-28 14:45:43 +0700

    update module
    update module menjadi 2 link

--- etiket_hotel.php
+++ etiket_hotel.php
@@ -13,11 +13,8 @@ if (isset($_POST["simpan"])) {
     $tanggal_lahir = $_POST["tanggal_lahir"];
     $nik = $_POST["nik"];
     $no_hp_penumpang = $_POST["no_hp_penumpang"];
-    $jenis_tiket = $_POST["jenis_tiket"];
-    $tanggal_berangkat = $_POST["tanggal_berangkat"];
-    $perkiraan_jam_keberangkatan = $_POST["perkiraan_jam_keberangkatan"];
-    $kota_asal = $_POST["kota_asal"];
     $kota_tujuan = $_POST["kota_tujuan"];
+    $nama_hotel = $_POST["nama_hotel"];
     $lampiran_email_approval = $_POST["lampiran_email_approval"];
 
     $pattern = "/[<`'~!$*,:|>]/";
@@ -29,12 +26,10 @@ if (isset($_POST["simpan"])) {
         $_SESSION["errormessage"] = "Mohon untuk NIK hanya Memasukan tulisan tanpa special karakter";
     } else if (preg_match($pattern, $no_hp_penumpang)) {
         $_SESSION["errormessage"] = "Mohon untuk Nomor Handphone Penumpang hanya Memasukan tulisan tanpa special karakter";
-    } else if (preg_match($pattern, $no_hp_pemesan)) {
-        $_SESSION["errormessage"] = "Mohon untuk Nomor Handphone Pemesan hanya Memasukan tulisan tanpa special karakter";
-    } else if (preg_match($pattern, $kota_asal)) {
-        $_SESSION["errormessage"] = "Mohon untuk Kota Asal hanya Memasukan tulisan tanpa special karakter";
     } else if (preg_match($pattern, $kota_tujuan)) {
         $_SESSION["errormessage"] = "Mohon untuk Kota Tujuan hanya Memasukan tulisan tanpa special karakter";
+    } else if (preg_match($pattern, $nama_hotel)) {
+        $_SESSION["errormessage"] = "Mohon untuk Nama Hotel hanya Memasukan tulisan tanpa special karakter";
     } else {
         $return = post_etiket_data($_POST);
     }
@@ -47,7 +42,7 @@ if (isset($_POST["simpan"])) {
             $_SESSION["message"] = "Request Anda berhasil disimpan, Mohon untuk Menunggu sampai Request bisa kami proses lebih lanjut, Terima Kasih.";
             javascript("setTimeout(function(){ window.location='https://corphr.com/'; }, 7500);");
         } else {
-            $_SESSION["errormessage"] = "Comprehensive Resume Anda Gagal tersimpan, Harap Coba Lagi atau hubungi CS kami.";
+            $_SESSION["errormessage"] = "Data Anda Gagal tersimpan, Harap Coba Lagi atau hubungi CS kami.";
         }
     }
 }
@@ -74,7 +69,7 @@ if (isset($_POST["simpan"])) {
             <div class="col-lg-9 col-md-12 col-xs-12">
                 <div class="post-job box">
                     <form method="POST" enctype="multipart/form-data" class="form-ad">
-                        <input type="hidden" name="mode" value="etiket">
+                        <input type="hidden" name="mode" value="etiket_hotel">
                         <div class="form-group">
                             <label class="control-label">Nama Lengkap (Nama Sesuai KTP)</label>
                             <input type="text" class="form-control" required title="Nama Lengkap" autocomplete="off" required id="nama" name="nama" value="<?= $_POST["nama"]; ?>" onkeypress="return isAlphabetsKey(event)" />
@@ -91,33 +86,21 @@ if (isset($_POST["simpan"])) {
                             <label class="control-label">Nomor Handphone Penumpang</label>
                             <input type="text" class="form-control" title="Nomor Handphone Penumpang" required autocomplete="off" name="no_hp_penumpang" value="<?= $_POST["no_hp_penumpang"]; ?>" onkeypress="return isNumberKey(event)" />
                         </div>
-                        <div class="form-group">
-                            <label class="control-label">Jenis Tiket</label>
-                            <div class="search-category-container">
-                                <label class="styled-select">
-                                    <select class="dropdown-product selectpicker" title="Jenis Tiket" required name="jenis_tiket" value="<?= $_POST["jenis_tiket"]; ?>">
-                                        <option <?php if ($_POST["jenis_tiket"] == "-") echo "selected"; ?> value="-">-</option>
-                                        <option <?php if ($_POST["jenis_tiket"] == "Tiket Pesawat") echo "selected"; ?> value="Tiket Pesawat">Tiket Pesawat</option>
-                                        <option <?php if ($_POST["jenis_tiket"] == "Tiket KA") echo "selected"; ?> value="Tiket KA">Tiket KA</option>
-                                    </select>
-                                </label>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label">Tanggal Berangkat</label>
-                            <input type="date" class="form-control" title="Tanggal Berangkat" autocomplete="off" required id="tanggal_berangkat" name="tanggal_berangkat" value="<?= $_POST["tanggal_berangkat"]; ?>" />
-                        </div>
-                        <div class=" form-group">
-                            <label class="control-label">Perkiraan Jam Keberangkatan</label>
-                            <input type="time" class="form-control" title="Perkiraan Jam Keberangkatan" autocomplete="off" required name="perkiraan_jam_keberangkatan" value="<?= $_POST["perkiraan_jam_keberangkatan"]; ?>" />
-                        </div>
-                        <div class=" form-group">
-                            <label class="control-label">Kota Asal</label>
-                            <input type="text" class="form-control" title="Kota Asal" autocomplete="off" required name="kota_asal" value="<?= $_POST["kota_asal"]; ?>" />
-                        </div>
                         <div class=" form-group">
                             <label class="control-label">Kota Tujuan</label>
                             <input type="text" class="form-control" title="Kota Tujuan" autocomplete="off" required name="kota_tujuan" value="<?= $_POST["kota_tujuan"]; ?>" />
+                        </div>
+                        <div class=" form-group">
+                            <label class="control-label">Nama Hotel</label>
+                            <input type="text" class="form-control" title="Nama Hotel Jam Keberangkatan" autocomplete="off" name="nama_hotel" value="<?= $_POST["nama_hotel"]; ?>" />
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Check In</label>
+                            <input type="date" class="form-control" title="Check In" autocomplete="off" required id="check_in" name="check_in" value="<?= $_POST["check_in"]; ?>" />
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Check Out</label>
+                            <input type="date" class="form-control" title="Check Out" autocomplete="off" required id="check_out" name="check_out" value="<?= $_POST["check_out"]; ?>" />
                         </div>
                         <label class="control-label" id="label_candidates_bank_account">Screenshot Lampiran Email Approval <?= $_txt_required; ?>
                             <br><b>&emsp;(Max Size:2MB )</b></label>
